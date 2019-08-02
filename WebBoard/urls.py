e64999bc1cb5c58124880400ae28598944717a2f
Shoaib <shoaib015muradnagar@gmail.com>
2019-08-03 00:55:32 +0530

    make it better

--- WebBoard/urls.py
+++ WebBoard/urls.py
@@ -3,22 +3,22 @@ from . import views
 from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-    url(r'', views.home, name="home"),
     # accounts related urls
-    url(r'^home/signup/$', views.signup, name="signup"),
+    url(r'^signup/$', views.signup, name="signup"),
     url(r'^accounts/login/$', login, {'template_name':'accounts/login.html'}, name="login"),
     url(r'^accounts/logout/$', logout, name="logout"),
     # app functionality related urls
-    url(r'^home/myposts/$', views.my_post, name="mypost"),
-    url(r'^home/questions/$', views.all_posts, name="allposts"),
-    url(r'^home/questions/mostviewed/$', views.most_viewed_post, name="mostviewed"),
-    url(r'^home/questions/(?P<pk>\d+)/like/$', views.likefunc, name="likefunc"),
-    url(r'^home/questions/ask/$', views.ask_question, name="askquestion"),
-    url(r'^home/questions/(?P<pk>\d+)/reply/$', views.reply_post, name="replypost"),
-    url(r'^home/questions/(?P<pk>\d+)/edit/$', views.edit_post, name="editpost"),
-    url(r'^home/questions/(?P<pk>\d+)/$', views.post, name="post"),
-    url(r'^home/topic/(?P<pk>\d+)/$', views.topicpostview, name="topicpostview"),
+    url(r'^myposts/$', views.my_post, name="mypost"),
+    url(r'^questions/$', views.all_posts, name="allposts"),
+    url(r'^questions/mostviewed/$', views.most_viewed_post, name="mostviewed"),
+    url(r'^questions/(?P<pk>\d+)/like/$', views.likefunc, name="likefunc"),
+    url(r'^questions/ask/$', views.ask_question, name="askquestion"),
+    url(r'^questions/(?P<pk>\d+)/reply/$', views.reply_post, name="replypost"),
+    url(r'^questions/(?P<pk>\d+)/edit/$', views.edit_post, name="editpost"),
+    url(r'^questions/(?P<pk>\d+)/$', views.post, name="post"),
+    url(r'^topic/(?P<pk>\d+)/$', views.topicpostview, name="topicpostview"),
     # user profile urls
-    url(r'^home/(?P<pk>\d+)/profile/$', views.profile, name="profile"),
-    url(r'^home/(?P<pk>\d+)/profile/edit/$', views.edit_profile, name="editprofile"),
+    url(r'^(?P<pk>\d+)/profile/$', views.profile, name="profile"),
+    url(r'^(?P<pk>\d+)/profile/edit/$', views.edit_profile, name="editprofile"),
+    url(r'', views.home, name="home"),
 ]
