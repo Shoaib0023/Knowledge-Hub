d343c150713197910cf6cca6d1320a7b53ba3a3d
Shoaib <shoaib015muradnagar@gmail.com>
2019-08-10 17:23:14 +0530

    final commit maybe

--- WebBoard/views.py
+++ WebBoard/views.py
@@ -44,8 +44,7 @@ def auth_login(request):
             return redirect('allposts')
     else:
         form = LoginForm()
-
-    return render(request, 'accounts/login.html', {'form': form})
+        return render(request, 'accounts/login.html', {'form': form})
 
 # def auth_logout(request):
 #     logout()
